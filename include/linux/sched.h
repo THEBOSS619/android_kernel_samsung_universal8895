3af77997777b47cc6de829a28ba2a3e429c29250
Park Bumgyu <bumgyu.park@samsung.com>
2018-03-22 14:43:33 +0900

    sched: change exynos scheduler name from EHMP to EMS
    Rename Exynos scheduler. Existing EHMP(Exynos HMP) was a scheduler
    considering only HMP chipset. EMS(Exynos Mobile Scheduler) will support
    all chipsets regardless of cluster and core configuration.
    
    Change-Id: I2802ddcd9e401a0d92f9c98656b5e591d429d6ce
    Signed-off-by: Park Bumgyu <bumgyu.park@samsung.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1394,7 +1394,6 @@ struct sched_avg {
 	struct util_est			util_est;
 };
 
-#ifdef CONFIG_SCHED_EHMP
 #define NOT_ONTIME		1
 #define ONTIME_MIGRATING	2
 #define ONTIME			4
@@ -1413,7 +1412,6 @@ struct ontime_entity {
 	int flags;
 	int cpu;
 };
-#endif
 
 #ifdef CONFIG_SCHEDSTATS
 struct sched_statistics {
@@ -1544,9 +1542,7 @@ struct sched_entity {
 	/* Per entity load average tracking */
 	struct sched_avg	avg;
 #endif
-#ifdef CONFIG_SCHED_EHMP
 	struct ontime_entity		ontime;
-#endif
 };
 
 struct sched_rt_entity {
